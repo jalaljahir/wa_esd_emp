53b9dd4669d97b91f92aed8a283b71cbf4494be7
jalaljahir <jalaljahir@hotmail.com>
2016-04-18 11:19:48 -0700

    Starting a script for monthly W&S employment update

---
+++ monthlyemp_ESD.py
@@ -0,0 +1,16 @@
+
+import urllib
+
+
+testfile = urllib.URLopener()
+testfile.retrieve("http://randomsite.com/file.gz", "file.gz")
+
+
+
+
+
+
+"https://fortress.wa.gov/esd/employmentdata/docs/economic-reports/wa-historical-employment-not-seasonally-adjusted.xlsx"
+
+"https://fortress.wa.gov/esd/employmentdata/docs/economic-reports/wa-historical-employment-seasonally-adjusted.xlsx"
+
