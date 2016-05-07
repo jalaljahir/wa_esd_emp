aca17ce4c45a0323a29ac7b8b6a07e8f57bad9e7
jalaljahir <jalaljahir@hotmail.com>
2016-05-07 02:06:23 -0700

    end of 05072016

--- monthlyemp_ESD.py
+++ monthlyemp_ESD.py
@@ -14,3 +14,42 @@ testfile.retrieve("http://randomsite.com/file.gz", "file.gz")
 
 "https://fortress.wa.gov/esd/employmentdata/docs/economic-reports/wa-historical-employment-seasonally-adjusted.xlsx"
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################
+
+#Date Time:
+
+#http://stackoverflow.com/questions/311627/how-to-print-date-in-a-regular-format-in-python
+
+# Time: http://stackoverflow.com/questions/7370801/measure-time-elapsed-in-python
+# Time: http://stackoverflow.com/questions/311627/how-to-print-date-in-a-regular-format-in-python
+ 
