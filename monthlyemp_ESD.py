a70f8b765d0bbd00cae157308a2fec86eb0e8fb2
jalaljahir <jalaljahir@hotmail.com>
2016-05-10 17:12:28 -0700

    work at office 05102016

--- monthlyemp_ESD.py
+++ monthlyemp_ESD.py
@@ -52,4 +52,17 @@ testfile.retrieve("http://randomsite.com/file.gz", "file.gz")
 
 # Time: http://stackoverflow.com/questions/7370801/measure-time-elapsed-in-python
 # Time: http://stackoverflow.com/questions/311627/how-to-print-date-in-a-regular-format-in-python
- 
+
+# Get-Current-Directory: 
+# http://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory 
+
+# http://stackoverflow.com/questions/3430372/how-to-get-full-path-of-current-files-directory-in-python
+
+# Download file using urllib:
+
+# http://stackoverflow.com/questions/19602931/basic-http-file-downloading-and-saving-to-disk-in-python
+
+# http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
+
+
+
