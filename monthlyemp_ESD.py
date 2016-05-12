0871f84673b8d6f659d70faf0133ee0428a1bb61
jalaljahir <jalaljahir@hotmail.com>
2016-05-11 23:23:32 -0700

    work at office 5-11-16

--- monthlyemp_ESD.py
+++ monthlyemp_ESD.py
@@ -64,5 +64,12 @@ testfile.retrieve("http://randomsite.com/file.gz", "file.gz")
 
 # http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
 
+#Delete rows in dataframe:
+
+# http://stackoverflow.com/questions/16396903/delete-first-three-rows-of-a-dataframe-in-pandas
+
+# delete columns in dataframe:
+
+# http://stackoverflow.com/questions/13411544/delete-column-from-pandas-dataframe
 
 
