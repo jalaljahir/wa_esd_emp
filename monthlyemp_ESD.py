4610031c29ee7707ac85c727372ae21cbd6baba9
jalaljahir <jalaljahir@hotmail.com>
2016-05-23 17:10:49 -0700

    work at office 05-23-2016

--- monthlyemp_ESD.py
+++ monthlyemp_ESD.py
@@ -72,12 +72,25 @@ testfile.retrieve("http://randomsite.com/file.gz", "file.gz")
 
 # http://stackoverflow.com/questions/16396903/delete-first-three-rows-of-a-dataframe-in-pandas
 
-# delete columns in dataframe:
+#delete columns in dataframe:
 
 # http://stackoverflow.com/questions/13411544/delete-column-from-pandas-dataframe
 
-# Rename Columns: http://stackoverflow.com/questions/11346283/renaming-columns-in-pandas
+#Rename Columns: http://stackoverflow.com/questions/11346283/renaming-columns-in-pandas
 
-# Sum rows in the dataframe:
+#Sum rows in the dataframe:
 
 # http://stackoverflow.com/questions/25748683/python-pandas-sum-dataframe-rows-for-given-columns
+
+#Merge multiple dataframes on columns:
+
+# http://stackoverflow.com/questions/23668427/pandas-joining-multiple-dataframes-on-columns
+
+# http://stackoverflow.com/questions/18792918/pandas-combining-2-data-frames-join-on-a-common-column
+
+#Write dataframe to existing excel file:
+
+# http://stackoverflow.com/questions/20219254/how-to-write-to-an-existing-excel-file-without-overwriting-data
+
+#delete worksheets from existing excel file:
+# http://stackoverflow.com/questions/24989506/python-2-7-i-cant-remove-worksheets-using-xlwt-workbook-workbook-worksheets 
