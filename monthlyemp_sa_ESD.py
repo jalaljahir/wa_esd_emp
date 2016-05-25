630401ce6516d58b240cdce96e186bda1062e91a
jalaljahir <jalaljahir@hotmail.com>
2016-05-24 17:04:15 -0700

    work at office 05-24-2016, working status .py files

--- monthlyemp_sa_ESD.py
+++ monthlyemp_sa_ESD.py
@@ -276,7 +276,7 @@ def data_to_workbook():
 
     sa_writer.save()
     
-    print "\nProcessed data are exported to the excel workbook located at the working directory:  "+"\n"+ os.getcwd() + "\nWorksheet Name: \n1. Region Master \n2. Region_Sector \n3. Region_CMonth \n4. Region_Sec_CMonth "
+    print "\nProcessed data are exported to the excel workbook located at the directory:  "+"\n"+ os.getcwd() + "\nWorksheet Name: \n1. Region Master \n2. Region_Sector \n3. Region_CMonth \n4. Region_Sec_CMonth "
 
 
 
@@ -311,54 +311,3 @@ print "\nProcessing Completed! \nProcessing Time: " + "{0: .3}".format(processti
 
 
 exit()
-
-###############
-
-#Date Time:
-
-#http://stackoverflow.com/questions/311627/how-to-print-date-in-a-regular-format-in-python
-
-# Time: http://stackoverflow.com/questions/7370801/measure-time-elapsed-in-python
-# Time: http://stackoverflow.com/questions/311627/how-to-print-date-in-a-regular-format-in-python
-
-# Date: http://stackoverflow.com/questions/25146121/extracting-just-month-and-year-from-pandas-datetime-column-python
-
-# Date time to String: http://stackoverflow.com/questions/22276503/how-to-i-change-data-type-of-pandas-data-frame-to-string-with-a-defined-format
-# Get-Current-Directory: 
-# http://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory 
-
-# http://stackoverflow.com/questions/3430372/how-to-get-full-path-of-current-files-directory-in-python
-
-# Download file using urllib:
-
-# http://stackoverflow.com/questions/19602931/basic-http-file-downloading-and-saving-to-disk-in-python
-
-# http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
-
-
-#Delete rows in dataframe:
-
-# http://stackoverflow.com/questions/16396903/delete-first-three-rows-of-a-dataframe-in-pandas
-
-#delete columns in dataframe:
-
-# http://stackoverflow.com/questions/13411544/delete-column-from-pandas-dataframe
-
-#Rename Columns: http://stackoverflow.com/questions/11346283/renaming-columns-in-pandas
-
-#Sum rows in the dataframe:
-
-# http://stackoverflow.com/questions/25748683/python-pandas-sum-dataframe-rows-for-given-columns
-
-#Merge multiple dataframes on columns:
-
-# http://stackoverflow.com/questions/23668427/pandas-joining-multiple-dataframes-on-columns
-
-# http://stackoverflow.com/questions/18792918/pandas-combining-2-data-frames-join-on-a-common-column
-
-#Write dataframe to existing excel file:
-
-# http://stackoverflow.com/questions/20219254/how-to-write-to-an-existing-excel-file-without-overwriting-data
-
-#delete worksheets from existing excel file:
-# http://stackoverflow.com/questions/24989506/python-2-7-i-cant-remove-worksheets-using-xlwt-workbook-workbook-worksheets 
